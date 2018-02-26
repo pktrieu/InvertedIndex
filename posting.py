c73d64b6106db678ad91664f38d93412fb95d9df
pktrieu <ptrieu96@gmail.com>
2018-02-25 18:42:24 -0800

    Posting object (may need tuning)

--- posting.py
+++ posting.py
@@ -1,2 +1,10 @@
+
 class Posting:
-    
+    def __init__(self, newUrl):
+        self.url = newUrl
+        self.frequency = {} # Dictionary of tokenized term in document/webpage
+        self.tf_idf = None # Score generated later on during search (?)
+
+
+
+    #Methods

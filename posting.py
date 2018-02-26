925409ca16f28f7d71e240bf7d638542adc6e7c5
pktrieu <ptrieu96@gmail.com>
2018-02-26 13:51:53 -0800

    Added title attribute to Posting object

--- posting.py
+++ posting.py
@@ -1,9 +1,11 @@
 
 class Posting:
-    def __init__(self, newUrl):
+    def __init__(self, newUrl, newTitle):
         self.url = newUrl
+        self. title = newTitle # May need title of page to make a more relevant search
         self.frequency = {} # Dictionary of tokenized term in document/webpage
         self.tf_idf = None # Score generated later on during search (?)
+
 
 
 
